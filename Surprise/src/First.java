90705b886b18e9e3accabd867aa81a2c4e8399c6
admin <admin@DESKTOP-LEHNTRB>
2019-10-09 12:39:48 +0530

    Surprise project

---
+++ Surprise/src/First.java
@@ -0,0 +1,7 @@
+import java.util.*; 
+public class First { 
+	public static void main(String args[]) { 
+		List<Object> list = new ArrayList<Object>(); 
+		list.add(10); //  the "E" in the "add(E)" call, was never specified. 
+	} 
+} 
